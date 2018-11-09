152a863ee5b60e5cf5d9210866b9e03bbe205cd8
jyothsnadeshpande <jyothsnajdeshpande@gmail.com>
2018-11-09 21:43:22 +0530

    Update flaskdb.py

--- DBMS Project/Flask/flaskdb.py
+++ DBMS Project/Flask/flaskdb.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for,flash, redirect
+from flask import Flask, render_template,url_for,flash,redirect,request
 from forms import RegistrationForm, LoginForm
 from database import *
 
@@ -15,6 +15,10 @@ def search_intake_service():
 	Director = request.form['director']
 	Genre = request.form['genre']
 	Year = request.form['year']
+	details = (Genre,Director,Actor,Year)
+	# details = (request.form['genre'],request.form['director'],request.form['actor'], request.form['year'])
+	# return (request.form['genre'],request.form['director'],request.form['actor'], request.form['year'])
+	return details
 	
 
 # def search_intake(service = 'null'):
@@ -26,53 +30,99 @@ def search_intake_service():
 
 def search_intake_name():
 	Name = request_form['name']	
-
+	return Name
 #=====================================================================================================================================
 
 @app.route("/")
 @app.route("/home")
 def home():
 	# search_intake_service()
-    return render_template('index.html') 
+	return render_template('index.html') 
 
 #=====================================================================================================    
 
 @app.route("/netflix")
 def netflix():
-    if(request.method == 'POST'):
-		search_intake_service()
-	print('Movies')
-	select_shows_all_cri(Genre,Director,Actor,Year)
-	print('Shows')
-	select_movies_all_cri(Genre,Director,Actor,Year)
+	details = ()
+	if(request.method == 'POST'):
+		details = search_intake_service()
+	# print('Movies')
+	select_shows_all_cri(details[0],details[1],details[2],details[3])
+	# (request.form['genre'],request.form['director'],request.form['actor'], request.form['year'])
+	# print('Shows')
+	select_movies_all_cri(details[0],details[1],details[2],details[3])
 
 	return render_template('netflix.html')
+
+
+@app.route("/netflixname")
+def netflix_name():
+	Name = ''
+	if(request.method == 'POST'):
+		Name = search_intake_name()
+	print('Movies\n')
+	select_movie_name(Name)
+	print('Shows')
+	select_show_name(Name)
+	
+	return render_template('netflix.html')	
 
 #======================================================================================================	
 
 @app.route("/amazon")
 def amazon():
+	details = ()
 	if(request.method == 'POST'):
-		search_intake_service()
-	print('Movies')
-	select_shows_all_cri(Genre,Director,Actor,Year)
+		details = search_intake_service()
+	print('Movies\n')
+	select_shows_all_cri(details[0],details[1],details[2],details[3])
 	print('Shows')
-	select_movies_all_cri(Genre,Director,Actor,Year)
+	select_movies_all_cri(details[0],details[1],details[2],details[3])
 
 	return render_template('amazon.html')
+
+
+
+@app.route("/amazonname")
+def amazon_name():
+	Name = ''
+	if(request.method == 'POST'):
+		Name = search_intake_name()
+	print('Movies\n')
+	select_movie_name(Name)
+	print('Shows')
+	select_show_name(Name)
+	
+	return render_template('amazon.html')	
+
 
 #=======================================================================================================
 
 @app.route("/hotstar")
 def hotstar():
+	details = ()
 	if(request.method == 'POST'):
-		search_intake_service()
-	print('Movies')
-	select_shows_all_cri(Genre,Director,Actor,Year)
-	print('Shows')
-	select_movies_all_cri(Genre,Director,Actor,Year)
+		details = search_intake_service()
+	# print('Movies')
+	data2 = select_shows_all_cri(details[0],details[1],details[2],details[3])
+	# print('Shows')
+	data1 = select_movies_all_cri(details[0],details[1],details[2],details[3])
+	
+	return render_template('hotstar.html',data1 = data1, data2 = data2)
 
-	return render_template('hotstar.html')
+
+@app.route("/hotstarname")
+def hotstar_name():
+	Name = ''
+	if(request.method == 'POST'):
+		Name = search_intake_name()
+	print('Movies\n')
+	select_movie_name(Name)
+	print('Shows')
+	select_show_name(Name)
+	
+	return render_template('hotstarname.html')	
+
 
 #=========================================================================================================	
 
