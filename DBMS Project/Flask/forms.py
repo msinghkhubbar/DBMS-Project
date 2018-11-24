443af174bec4e616fb05b89bd4635d82d27868c9
msinghkhubbar <43778674+msinghkhubbar@users.noreply.github.com>
2018-11-24 23:41:43 +0530

    Add files via upload

--- DBMS Project/Flask/forms.py
+++ DBMS Project/Flask/forms.py
@@ -4,19 +4,23 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 import database
 
 
-def name_exists(form, field):
-    if (field.data in database.select_users()):
-        raise ValidationError('Email already exists.')
+def name_exists_register(form, field):
+	if (field.data in database.select_users()):
+		raise ValidationError('Email already exists.')
+
+def name_exists_login(form, field):
+	if (field.data not in database.select_users()):
+		raise ValidationError("Email doesn't exist. Check email or register")
 
 class RegistrationForm(FlaskForm):
 	firstname = StringField('First Name *', validators=[DataRequired()])
 	lastname = StringField('Last Name *', validators=[DataRequired()])
-	email = StringField('Email *', validators=[DataRequired(), Email()])
+	email = StringField('Email *', validators=[DataRequired(), Email(), name_exists_register])
 	password = PasswordField('Password *',validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm password *',validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign up')
 
 class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[DataRequired(), Email()])
+	email = StringField('Email', validators=[DataRequired(), Email(), name_exists_login])
 	password = PasswordField('Password',validators=[DataRequired()])
 	submit = SubmitField('Login')
